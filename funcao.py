144ef40b88595d7bd5bb62d4ef8182adc278f410
gabrielpradoesps <gabriel.espirito@aluno.impacta.edu.br>
2026-09-10 22:09:06 -0300

    Add files via upload

---
+++ funcao.py
@@ -0,0 +1,29 @@
+def somar (n1, n2):
+    resultado = n1 + n2
+    return resultado
+
+soma = somar (5, 3)
+
+
+def subtrair (n1, n2):
+    resultado = n1 - n2
+    return resultado
+
+subtrai = subtrair (10, 3)
+
+def multiplicar (n1, n2):
+    resultado = n1 * n2
+    return resultado
+
+multiplica = multiplicar (2, 5)
+
+def dividir (n1, n2):
+    resultado = n1 / n2
+    return resultado
+
+divide = dividir (20, 10)
+
+print("soma", soma)
+print("subtração", subtrai)
+print("multiplicação", multiplica)
+print("divisão", divide)
---
+++ funcao2.py
@@ -0,0 +1,15 @@
+def calcular_media (lista_de_numeros):
+
+    total = sum(lista_de_numeros)
+    quantidade = len(lista_de_numeros)
+
+    if quantidade == 0:
+        return 0
+
+    media = total / quantidade
+    return media
+
+aluno1 = [6.5, 7.0, 8.0, 9.5, 9.5]
+media_aluno1 = calcular_media(aluno1)
+
+print("A média é: ", media_aluno1)
